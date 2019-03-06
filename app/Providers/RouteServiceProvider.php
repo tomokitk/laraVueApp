c28da63766042480df0c15093e54c46a63b3a650
kuriokatomoki <kuritomo0512@gmail.com>
2019-03-06 15:46:24 +0900

    ログイン画面作成

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,9 +65,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+    Route::prefix('api')
+         ->middleware('web') // ★ 'api' → 'web' に変更
+         ->namespace($this->namespace)
+         ->group(base_path('routes/api.php'));
     }
 }
