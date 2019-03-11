a2e279901d1a2b7ad3fdedd4e2e7ab174174c776
kuriokatomoki <kuritomo0512@gmail.com>
2019-03-11 15:57:18 +0900

    errorsHandling

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,5 +1,9 @@
+import { OK, CREATED, UNPROCESSABLE_ENTITY } from '../util'
 const state = {
-    user: null
+    user: null,
+    apiStatus: null,
+    loginErrorMessages: null,
+    registerErrorMessages: null
   }
   
   const getters = {
@@ -10,6 +14,17 @@ const state = {
   const mutations = {
     setUser (state, user) {
       state.user = user
+    },
+    setLoginErrorMessages (state, messages) {
+        state.loginErrorMessages = messages
+        console.log('setLoginErrorMessages')
+        console.log(state.loginErrorMessages)
+    },
+    setApiStatus (state, status) {
+        state.apiStatus = status
+    },
+    setRegisterErrorMessages (state, messages) {
+        state.registerErrorMessages = messages
     }
   }
   
@@ -17,22 +32,71 @@ const state = {
     async register (context, data) {
       const response = await axios.post('/api/register', data)
       context.commit('setUser', response.data)
+
+      if (response.status === CREATED) {
+        context.commit('setApiStatus', true)
+        context.commit('setUser', response.data)
+        return false
+      }
+  
+      context.commit('setApiStatus', false)
+      if (response.status === UNPROCESSABLE_ENTITY) {
+        console.log('ffffffff')
+        context.commit('setRegisterErrorMessages', response.data.errors)
+      } else {
+        context.commit('error/setCode', response.status, { root: true })
+      }
+
     },
     async login (context, data) {
-      const response = await axios.post('/api/login', data)
-      context.commit('setUser', response.data)
+        context.commit('setApiStatus', null)
+        const response = await axios.post('/api/login', data).catch(err => err.response || err)
+      
+        if (response.status === OK) {
+          context.commit('setApiStatus', true)
+          context.commit('setUser', response.data)
+          return false
+        }
+      
+        context.commit('setApiStatus', false)
+        if (response.status === UNPROCESSABLE_ENTITY) {
+            console.log('auth.js')
+            console.log(response.status)
+            console.log(response.data.errors)
+            context.commit('setLoginErrorMessages', response.data.errors)
+          } else {
+            context.commit('error/setCode', response.status, { root: true })
+        }
     },
     async logout (context) {
-      const response = await axios.post('/api/logout')
-      context.commit('setUser', null)
+        context.commit('setApiStatus', null)
+        const response = await axios.post('/api/logout')
+    
+        if (response.status === OK) {
+         console.log('logout')
+          context.commit('setApiStatus', true)
+          context.commit('setUser', null)
+          return false
+        }
+    
+        context.commit('setApiStatus', false)
+        context.commit('error/setCode', response.status, { root: true })
     },
     async currentUser (context) {
-      const response = await axios.get('/api/user')
-      const user = response.data || null
-      context.commit('setUser', user)
-    }
+        context.commit('setApiStatus', null)
+        const response = await axios.get('/api/user')
+        const user = response.data || null
+    
+        if (response.status === OK) {
+          context.commit('setApiStatus', true)
+          context.commit('setUser', user)
+          return false
+        }
+    
+        context.commit('setApiStatus', false)
+        context.commit('error/setCode', response.status, { root: true })
+      }
   }
-  
   export default {
     namespaced: true,
     state,
